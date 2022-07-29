676c9e40346ee47350c92fb602a94e6976fbbdfe
olega211 <lexerdxx@gmail.com>
2022-07-30 04:15:46 +0700

    f

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('‘express');
+const https = require('https');
+const fs = require('fs');
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/fullchain.pem')
+// };
+const app = express();
+app.use((req, res) =>
+{
+    res.end('Hello World');
+});
+
+https.createServer(options, app).listen(8000);
